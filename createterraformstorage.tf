a8961fbb27f56dd428aec026b50e21618b830a2f
jtanaeki24 <141356601+jtanaeki24@users.noreply.github.com>
2026-05-25 06:56:54 -0400

    initial commit

---
+++ createterraformstorage.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "devopsrg" {
+  name     = "terraform-devops-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storageterr" {
+  name                     = "teststorageaccount098765"
+  resource_group_name      = azurerm_resource_group.devopsrg.name
+  location                 = azurerm_resource_group.devopsrg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  public_network_access_enabled = true
+
+#  tags = {
+#    environment = "dev"
+#  }
+}
